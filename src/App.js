b69a399c0706f87e0ebaddb9d806023245c99d5f
Md. Mehedi Hasan <71551839+MehediKhokon@users.noreply.github.com>
2020-12-21 02:24:40 +0600

    initialized recipe app
    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Recipes from './components/Recipes';
+import axios from 'axios'
+
+const url = 'https://django-recipe-app.herokuapp.com'
 
 function App() {
+  const [recipes, setRecipes] = useState([])
+
+  async function fetchData(){
+    const res = await axios.get(url)
+    const data = res.data
+    setRecipes(data)
+    console.log(data)
+  }
+
+  useEffect(()=>{
+    fetchData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container" >
+		    <div className="title text-center sticky-top mt-5">
+			    <h1><span>Recipe APP</span></h1>
+		    </div>
+		    {recipes.map((recipe)=>{
+          return(
+            <Recipes key={recipe.id} {...recipe} />
+          )
+        })}    
+	    </div>
+      <footer className="text-center text-white">
+        @no right 
+      </footer>
+    </>
+
   );
 }
 
---
+++ src/components/Recipes.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+function Recipes({title, description, created}) {
+    const [more, setMore] = useState(false)
+    return (
+        <div className="card mb-3 mt-5">	  
+	  		<div className="card-body">
+	    		<h5 className="card-title">{title}</h5>
+	    		<p className="card-text">
+                    {more ? description: `${description.substring(0,250)}...`}
+                    <button className="btn" onClick={()=> setMore(!more)}>
+                        {more ? 'show less': 'show more'}
+                    </button>
+                </p>
+	    		<p className="card-text"><small className="text-muted">Date Added: {created.substring(0,10)}</small></p>
+	  		</div>
+		  </div>
+    )
+}
+
+export default Recipes
