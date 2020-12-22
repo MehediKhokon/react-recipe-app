fb8062ed1ead4e7495a8266ce8cb89277635298a
Md. Mehedi Hasan <71551839+MehediKhokon@users.noreply.github.com>
2020-12-23 02:26:59 +0600

    correction

--- src/components/SkeletonCard.js
+++ src/components/SkeletonCard.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import Skeleton from 'react-loading-skeleton'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 
 function SkeletonCard() {
     return (
-    <div>
+    <SkeletonTheme color="#97a19e" highlightColor="#00ffb3">
+
         {Array(3).fill().map((item, index)=>{
             return(
                 <div key={index} className="card mb-3 mt-5">	  
 	  		        <div className="card-body">
-	    		        <h5 className="card-title"><Skeleton duration={2} /></h5>
+	    		        <h5 className="card-title"><Skeleton duration={1.5} /></h5>
 	    		        <p className="card-text">
-                            <Skeleton duration={2} count={2} />
+                            <Skeleton duration={1.5} count={2} />
                         </p>
-	    		        <p className="card-text"><small className="text-muted"><Skeleton duration={2} width={200} /></small></p>
+	    		        <p className="card-text"><small className="text-muted"><Skeleton duration={1.5} width={220} /></small></p>
 	  		        </div>
 		        </div>
             )
         })}
-    </div>
+    </SkeletonTheme>
             
     )
 }
